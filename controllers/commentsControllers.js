87e0f5afd5e5263496a9351cc541975b6274f462
Olumorothy <iseoluwa89@gmail.com>
2022-10-13 11:15:07 +0100

    responds with a newly posted comment by article_id

--- controllers/commentsControllers.js
+++ controllers/commentsControllers.js
@@ -1,5 +1,8 @@
-const fetchArticleById = require("../models/articlesModel");
-const fetchCommentsByArticleId = require("../models/commentsModel");
+const { fetchArticleById } = require("../models/articlesModel");
+const {
+  fetchCommentsByArticleId,
+  insertComment,
+} = require("../models/commentsModel");
 
 function getCommentsByArticleId(req, res, next) {
   const { article_id } = req.params;
@@ -12,4 +15,20 @@ function getCommentsByArticleId(req, res, next) {
     });
 }
 
-module.exports = getCommentsByArticleId;
+function addCommentByArticleId(req, res, next) {
+  const { article_id } = req.params;
+  const { username, body } = req.body;
+  //first check if article exist.
+  const promises = [
+    fetchArticleById(article_id),
+    insertComment(username, body, article_id),
+  ];
+  Promise.all(promises)
+    .then((promise) => {
+      res.status(201).send({ comment: promise[1] });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+module.exports = { getCommentsByArticleId, addCommentByArticleId };
